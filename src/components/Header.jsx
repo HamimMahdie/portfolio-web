714fc3c3efa1ca8211e1a695ff62175967f9c60e
Hamim Mahdie <144726390+HamimMahdie@users.noreply.github.com>
2024-09-19 02:59:18 -0400

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -44,7 +44,7 @@ const Header = () => {
                     <br/>
                     <p className="mb-4 text-lg">Hi, I'm Hamim Mahdie from Hartford, CT. 
                         <br/>
-                        From aspiring to be a 'cool' hacker to printing countless 'Hello World's, I found my calling for programming and computer science. Check out my latest project here 
+                        From aspiring to be a 'cool' hacker to printing countless 'Hello World's, I found my calling for programming and computer science. <br></br>Check out my latest project here 
                         <a href="https://stocker-finance.netlify.app/" target="_blank" rel="noopener noreferrer" className="text-theme-red hover:underline"> Stocker Finance</a>
                      </p>
                         <br/>
